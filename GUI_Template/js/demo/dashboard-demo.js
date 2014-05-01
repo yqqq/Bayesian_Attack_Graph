8b89824f433261ed9081ac2821c1d4c6f38d9978
lovingmage <skiphoopwow@gmail.com>
2014-05-01 14:22:21 +0800

    I Fitstly Get the DIV of first paper now is the finish work
    Finish Work for yqqq and DKX

--- GUI_Template/js/demo/dashboard-demo.js
+++ GUI_Template/js/demo/dashboard-demo.js
@@ -113,5 +113,21 @@ $(function() {
         hideHover: 'auto',
         resize: true
     });
+	Morris.Line({
+ 		 // ID of the element in which to draw the chart.
+ 		 element: 'myfirstchart',
+ 		 // Chart data records -- each entry in this array corresponds to a point on
+ 		 // the chart.
+		  data: [
+			{ year: '2008', value: 20 },
+			{ year: '2009', value: 10 },
+			{ year: '2010', value: 5 },
+			{ year: '2011', value: 5 },
+			{ year: '2012', value: 20 }
+ 				 ],
+		xkey: 'year',
+		ykeys: ['value'],
+	    labels: ['Value']
+	});
 
 });
