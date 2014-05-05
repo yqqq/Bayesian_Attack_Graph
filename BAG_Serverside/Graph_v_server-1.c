3792219696c19578ac3ea07e4ae85844d11602a1
lovingmage <skiphoopwow@gmail.com>
2014-05-05 20:51:37 +0800

    Server Side Socket Establish
    We got the new Socket Deamon process with Net work Conn

--- BAG_Serverside/Graph_v_server-1.c
+++ BAG_Serverside/Graph_v_server-1.c
@@ -8,6 +8,12 @@
 #include<sys/stat.h>
 #include<sys/param.h>
 #include<time.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+#define MAXLINE 4096
 
 /*--------------------------------------------------*/
 /*init_deamon function will set up a deamon process;*/
@@ -26,9 +32,9 @@ void init_deamon(void){
         }
     else if(pid<0){
     /*THIS PLACE SHOULD INSERT LOG HANDLER*/
-    	if((fp=fopen("/home/lovingmage/Desktop/Graph_Attack/Graph_Error.log","a"))>=0)
+    	if((fp=fopen("Graph_Error.log","a"))>=0)
         {
-            t=time(0);
+            log_time=time(0);
             fprintf(fp,"%s ->INIT DEAMON PROCESS FIRST STEP ERROR.",asctime(localtime(&log_time)));
             fclose(fp);
         }
@@ -44,9 +50,9 @@ void init_deamon(void){
             exit(0);
     else if(pid<0)
     /*THIS PLACE SHOULD INSERT LOG HANDLER*/
-    	if((fp=fopen("/home/lovingmage/Desktop/Graph_Attack/Graph_Error.log","a"))>=0)
+    	if((fp=fopen("Graph_Error.log","a"))>=0)
         {
-            t=time(0);
+            log_time=time(0);
             fprintf(fp,"%s ->INIT DEAMON PROCESS SECOND STEP ERROR.",asctime(localtime(&log_time)));
             fclose(fp);
         }
@@ -59,9 +65,9 @@ void init_deamon(void){
     chdir("/tmp");
     /*Reset the file mask*/
     umask(0);
-    if((fp=fopen("/home/lovingmage/Desktop/Graph_Attack/Graph.log","a"))>=0)
+    if((fp=fopen("Graph.log","a"))>=0)
         {
-            t=time(0);
+            log_time=time(0);
             fprintf(fp,"%s ->DEAMON PROCESS INIT SUCESSFULLY.",asctime(localtime(&log_time)));
             fclose(fp);
         }
@@ -73,7 +79,8 @@ int main(int argc,char *argv){
     time_t log_time;
     init_deamon();
 	/*This place is the fork of the program & Then to excute another function*/
-	/*------------------------------------------------------------------------
+	/*
+     ------------------------------------------------------------------------
 	int status;
 	pid_t chpid;
 	chpid=fork();
@@ -87,11 +94,11 @@ int main(int argc,char *argv){
 	else {
 	waitpid(chpid,&status,0);
 	}
-	---------------------------------------------------------------------------*/
+	---------------------------------------------------------------------------
+    */
     while(1)
     {
-        sleep(20);
-        
+        //sleep(20);
         /*
         if((fp=fopen("/home/lovingmage/Desktop/metasploitavevasion-master/Graph.log","a"))>=0)
         {
@@ -100,6 +107,75 @@ int main(int argc,char *argv){
             fclose(fp);
             }
         */
+        
+        /*-----------------------------------/
+        /Set init vars of socket connection  /
+        /               START                /
+        /-----------------------------------*/
+        int listenfd,connfd;
+        struct sockaddr_in servaddr;
+        char buff[4096];
+        int n;
+        
+        /*END of socket addrs init*/
+        /*Create init socket function*/
+        if(listenfd=socket(AF_INET,SOCK_STREAM,0)==-1){
+            if((fp=fopen("Graph_Error.log","a"))>=0)
+            {
+                log_time=time(0);
+                fprintf(fp,"%s ->INIT DEAMON SOCKET LISTENFD ERROR.",asctime(localtime(&log_time)));
+                fclose(fp);
+            }
+
+        }
+        
+        /*Establish & mem all addr struct*/
+        memset(&servaddr,0,sizeof(servaddr));
+        servaddr.sin_family=AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port   =   htons(6666);
+        
+        if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr))==-1){
+            //Error Log record the banner
+            
+            if((fp=fopen("Graph_Error.log","a"))>=0)
+            {
+                log_time=time(0);
+                fprintf(fp,"%s ->SOCKET BIND ERROR.",asctime(localtime(&log_time)));
+                fclose(fp);
+            }
+        }
+        
+        /*Ready for Accept Connection*/
+        
+        if((fp=fopen("Graph.log","a"))>=0)
+        {
+            log_time=time(0);
+            fprintf(fp,"%s ->Ready for Accept out connection Server connection Estavlish.",asctime(localtime(&log_time)));
+            fclose(fp);
+        }
+        
+        while(1){
+            if(connfd=(accept(listenfd,(struct sockaddr*)NULL,NULL))==-1){
+                if((fp=fopen("Graph_Error.log","a"))>=0)
+                {
+                    log_time=time(0);
+                    fprintf(fp,"%s ->ACCEPT CONNECT ERROR.",asctime(localtime(&log_time)));
+                    fclose(fp);
+                }
+                continue;
+            }
+            n = recv(connfd, buff,MAXLINE,0);
+            if((fp=fopen("Socket.log","a"))>=0)
+            {
+                log_time=time(0);
+                fprintf(fp,buff,asctime(localtime(&log_time)));
+                fclose(fp);
+            }
+
+            close(connfd);
+        }
+        close(listenfd);
      }
      return 0;
 }
