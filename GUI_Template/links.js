8de2b76684cdade1d8488b1129172bec5970c404
myshowhello <987732145@qq.com>
2014-05-12 15:59:20 +0800

    fisheyes
    add the fisheye into the blank.html

---
+++ GUI_Template/links.js
@@ -0,0 +1,14 @@
+var links = [  
+  {source: "Duankaixuan", target: "Amazon", type: "licensing"},  
+  {source: "Duankaixuan", target: "HTC", type: "licensing"},  
+  {source: "Samsung", target: "xiao b", type: "suit"},  
+  {source: "Motorola", target: "xiao b", type: "licensing2"},  
+  {source: "HTC", target: "xiao b", type: "suit"},    
+  {source: "Duankaixuan", target: "Barnes & Noble", type: "resolved"},  
+  {source: "Duankaixuan", target: "Foxconn", type: "licensing"},    
+  {source: "xiao b", target: "HTC", type: "licensing"},  
+  {source: "Duankaixuan", target: "Inventec", type: "licensing1"},        
+  {source: "xiao b", target: "Motorola", type: "resolved"},  
+  {source: "Duankaixuan", target: "Motorola", type: "suit"},  
+  {source: "Motorola", target: "Duankaixuan", type: "suit"}
+];  
