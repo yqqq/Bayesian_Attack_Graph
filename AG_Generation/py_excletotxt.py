d5ca7c64ce8c183df31884f5ff61900effd38557
yqqq <2509365019@qq.com>
2014-05-14 13:12:51 +0800

    mysql input
    change excle to txt

---
+++ AG_Generation/py_excletotxt.py
@@ -0,0 +1,24 @@
+import xlrd     
+#read excle need lib xlrd
+
+data=xlrd.open_workbook('BAGRA.xls')
+#open excle
+
+table=data.sheets()[0]
+#select sheet
+
+files=open('BAGRA.txt','w')
+#open txt
+
+for i1 in range(table.nrows):
+    rows=table.row_values(i1)
+    for i2 in range(0,5):
+        str1=rows[i2]
+        files.write(str(str1))
+        files.write('\t')
+    files.write('\r\n')
+
+files.close()
+
+    
+    
